038ccb9f8acef7be31bf9c019ae294d4edabb97b
SchwiDolaUwU <junichi910839539@gmail.com>
2024-02-18 05:04:22 -0800

    Add user location, added alert music

--- Interactive Button Instance with Map/app/src/main/java/com/example/drones/Alert.kt
+++ Interactive Button Instance with Map/app/src/main/java/com/example/drones/Alert.kt
@@ -1,6 +1,8 @@
 package com.example.drones
 
 import android.widget.Toast
+import android.media.MediaPlayer;
+import android.content.Context
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
@@ -13,52 +15,57 @@ import java.util.TimerTask
 
 
 class Alert {
+    private var mediaPlayer: MediaPlayer? = null
     val timer = Timer()
     var alert_on = false
     var map_on = false
     var fragment_map : GoogleMap?=null
     var fragment_map_set = false
     var fragment_marker_on = false
+    var musicPlayable = true
     fun fetchAircraftData(mainActivity:MainActivity, radius: Double) {
-            val timerTask = object : TimerTask() {
-                override fun run() {
+        val timerTask = object : TimerTask() {
+            override fun run() {
 
-                    if (alert_on or fragment_marker_on) {
-                        if (fragment_map_set && fragment_map != null) {
-                            mainActivity.runOnUiThread {
-                                fragment_map?.clear()
-                            }
-                            /**
-                            fragment_map?.addMarker(
-                                MarkerOptions().position(LatLng(lat, lon)).title("My Position")
-                            )
-                            **/
+                if (alert_on or fragment_marker_on) {
+                    if (fragment_map_set && fragment_map != null) {
+                        mainActivity.runOnUiThread {
+                            fragment_map?.clear()
                         }
-                        val call = RetrofitClient.instance.getAircraftData(mainActivity.getLat(), mainActivity.getLon(), radius)
-                        call.enqueue(object : Callback<AircraftResponse> {
-                            override fun onResponse(
-                                call: Call<AircraftResponse>,
-                                response: Response<AircraftResponse>
-                            ) {
-
-                                response.body()?.ac?.forEach { aircraft ->
-                                    if (alert_on) {
-                                        alertFunction(aircraft, mainActivity)
-                                    }
-                                    if (fragment_marker_on) {
-                                        fragmentMapFunction(aircraft)
-                                    }
+                        /**
+                        fragment_map?.addMarker(
+                        MarkerOptions().position(LatLng(lat, lon)).title("My Position")
+                        )
+                         **/
+                    }
+                    val call = RetrofitClient.instance.getAircraftData(mainActivity.getLat(), mainActivity.getLon(), radius)
+                    call.enqueue(object : Callback<AircraftResponse> {
+                        override fun onResponse(
+                            call: Call<AircraftResponse>,
+                            response: Response<AircraftResponse>
+                        ) {
+                            musicPlayable = true
+                            response.body()?.ac?.forEach { aircraft ->
+                                if (alert_on) {
+                                    alertFunction(aircraft, mainActivity, 5)
+                                }
+                                if (fragment_marker_on) {
+                                    fragmentMapFunction(aircraft)
+                                    fragment_map?.addMarker(
+                                        MarkerOptions().position(LatLng(mainActivity.getLat(), mainActivity.getLon())).title("your position")
+                                    )
                                 }
                             }
+                        }
 
-                            override fun onFailure(call: Call<AircraftResponse>, t: Throwable) {
-                                //Toast.makeText(GoogleMap, "fail to fetch the data", Toast.LENGTH_SHORT).show()
-                            }
-                        })
-                    }
+                        override fun onFailure(call: Call<AircraftResponse>, t: Throwable) {
+                            //Toast.makeText(GoogleMap, "fail to fetch the data", Toast.LENGTH_SHORT).show()
+                        }
+                    })
                 }
             }
-            timer.scheduleAtFixedRate(timerTask, 0, 5000)
+        }
+        timer.scheduleAtFixedRate(timerTask, 0, 5000)
     }
     fun startAlertDetection()
     {
@@ -83,15 +90,24 @@ class Alert {
         fragment_marker_on = true
     }
 
-    fun alertFunction(aircraft: Aircraft,mainActivity:MainActivity){
+    fun alertFunction(aircraft: Aircraft,mainActivity:MainActivity, alertRange : Int){
         val alat: String = String.format("%.3f", aircraft.lat)
         val alon: String = String.format("%.3f", aircraft.lon)
-        Toast.makeText(
-            mainActivity.getApplicationContext(),
-            alat + " " + alon,
-            Toast.LENGTH_SHORT
-        )
-            .show()
+        var d = distance(mainActivity.getLat(), mainActivity.getLon(), aircraft.lat, aircraft.lon)
+        if (d < alertRange)
+        {
+            Toast.makeText(
+                mainActivity.getApplicationContext(),
+                alat + " " + alon,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+            if (musicPlayable)
+            {
+                playAlertSound(mainActivity)
+                musicPlayable = false
+            }
+        }
     }
 
     fun fragmentMapFunction(aircraft: Aircraft){
@@ -99,5 +115,33 @@ class Alert {
         fragment_map?.addMarker(
             MarkerOptions().position(one_aircraft).title("one aircraft").icon(BitmapDescriptorFactory.fromAsset("airplane_icon.png"))
         )
+    }
+
+    fun playAlertSound(context: Context) {
+
+        mediaPlayer?.release()
+
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(context, R.raw.alert)
+        mediaPlayer.setOnCompletionListener {
+            it.release()
+        }
+        mediaPlayer.start()
+    }
+
+    private fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val theta = lon1 - lon2
+        var dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta))
+        dist = Math.acos(dist)
+        dist = rad2deg(dist)
+        dist = dist * 60 * 1.1515
+        return dist / 1.1508
+    }
+
+    private fun deg2rad(deg: Double): Double {
+        return (deg * Math.PI / 180.0)
+    }
+
+    private fun rad2deg(rad: Double): Double {
+        return (rad * 180.0 / Math.PI)
     }
 }
